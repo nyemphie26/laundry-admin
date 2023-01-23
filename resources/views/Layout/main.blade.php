fad041a3180620859b03d8db0daa3e24f85156fe
nyemphie26 <s.hidayatullah1726@gmail.com>
2023-01-23 21:01:02 +0700

    Change web title

--- resources/views/Layout/main.blade.php
+++ resources/views/Layout/main.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>{{ env('APP_NAME') }}</title>
 
         <!--     Fonts and icons     -->
         <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />

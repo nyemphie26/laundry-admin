43b6bcda64f39ebe6b0f4a5b683a1ef72c7f461c
nyemphie26 <s.hidayatullah1726@gmail.com>
2023-01-26 16:05:03 +0700

    update dashboard page

--- resources/views/Pages/dashboard.blade.php
+++ resources/views/Pages/dashboard.blade.php
@@ -3,7 +3,139 @@
 
 @section('content')
     <div class="col-lg-12 position-relative z-index-2">
-        <div class="card mb-4 ">
+        <div class="row mb-4">
+            <div class="col-lg-3 col-md-6 col-sm-6">
+                <div class="card  mb-2">
+                    <div class="card-header p-3 pt-2">
+                        <div class="icon icon-lg icon-shape bg-gradient-primary shadow-primary shadow text-center border-radius-xl mt-n4 position-absolute">
+                        <i class="material-icons opacity-10">leaderboard</i>
+                        </div>
+                        <div class="text-end pt-1">
+                        <p class="text-sm mb-0 text-capitalize">New Orders</p>
+                        <h4 class="mb-0">10</h4>
+                        </div>
+                    </div>
+                    <hr class="dark horizontal my-0">
+                    <div class="card-footer p-3">
+                        <p class="mb-0"><a href="{{ route('orders.incoming') }}" class="text-primary"> Click here</a> to see the orders</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6 mt-sm-0 mt-4">
+                <div class="card  mb-2">
+                    <div class="card-header p-3 pt-2">
+                        <div class="icon icon-lg icon-shape bg-gradient-dark shadow-dark shadow text-center border-radius-xl mt-n4 position-absolute">
+                        <i class="material-icons opacity-10">local_laundry_service</i>
+                        </div>
+                        <div class="text-end pt-1">
+                        <p class="text-sm mb-0 text-capitalize">On process orders</p>
+                        <h4 class="mb-0">35</h4>
+                        </div>
+                    </div>
+                    <hr class="dark horizontal my-0">
+                    <div class="card-footer p-3">
+                        <p class="mb-0"><a href="{{ route('orders.list') }}" class="text-primary"> Click here</a> to see the order list</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6 mt-lg-0 mt-4">
+                <div class="card  mb-2">
+                <div class="card-header p-3 pt-2 bg-transparent">
+                    <div class="icon icon-lg icon-shape bg-gradient-success shadow-success text-center border-radius-xl mt-n4 position-absolute">
+                    <i class="material-icons opacity-10">store</i>
+                    </div>
+                    <div class="text-end pt-1">
+                    <p class="text-sm mb-0 text-capitalize ">Revenue</p>
+                    <h4 class="mb-0 ">34k</h4>
+                    </div>
+                </div>
+                <hr class="horizontal my-0 dark">
+                <div class="card-footer p-3">
+                    <p class="mb-0 "><span class="text-success text-sm font-weight-bolder">+105% </span>than last month</p>
+                </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6 mt-lg-0 mt-4">
+                <div class="card ">
+                <div class="card-header p-3 pt-2 bg-transparent">
+                    <div class="icon icon-lg icon-shape bg-gradient-info shadow-info text-center border-radius-xl mt-n4 position-absolute">
+                    <i class="material-icons opacity-10">person_add</i>
+                    </div>
+                    <div class="text-end pt-1">
+                    <p class="text-sm mb-0 text-capitalize ">Customer</p>
+                    <h4 class="mb-0 ">+91</h4>
+                    </div>
+                </div>
+                <hr class="horizontal my-0 dark">
+                <div class="card-footer p-3">
+                    <p class="mb-0 ">Just updated</p>
+                </div>
+                </div>
+            </div>
+        </div>
+        <div class="row mb-4">
+        <div class="col-lg-4 col-md-6 mt-4 mb-4">
+            <div class="card z-index-2 ">
+            <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2 bg-transparent">
+                <div class="bg-gradient-primary shadow-primary border-radius-lg py-3 pe-1">
+                <div class="chart">
+                    <canvas id="chart-bars" class="chart-canvas" height="170"></canvas>
+                </div>
+                </div>
+            </div>
+            <div class="card-body">
+                <h6 class="mb-0 ">Website Views</h6>
+                <p class="text-sm ">Last Campaign Performance</p>
+                <hr class="dark horizontal">
+                <div class="d-flex ">
+                <i class="material-icons text-sm my-auto me-1">schedule</i>
+                <p class="mb-0 text-sm"> campaign sent 2 days ago </p>
+                </div>
+            </div>
+            </div>
+        </div>
+        <div class="col-lg-4 col-md-6 mt-4 mb-4">
+            <div class="card z-index-2  ">
+            <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2 bg-transparent">
+                <div class="bg-gradient-success shadow-success border-radius-lg py-3 pe-1">
+                <div class="chart">
+                    <canvas id="chart-line" class="chart-canvas" height="170"></canvas>
+                </div>
+                </div>
+            </div>
+            <div class="card-body">
+                <h6 class="mb-0 "> Daily Sales </h6>
+                <p class="text-sm "> (<span class="font-weight-bolder">+15%</span>) increase in today sales. </p>
+                <hr class="dark horizontal">
+                <div class="d-flex ">
+                <i class="material-icons text-sm my-auto me-1">schedule</i>
+                <p class="mb-0 text-sm"> updated 4 min ago </p>
+                </div>
+            </div>
+            </div>
+        </div>
+        <div class="col-lg-4 mt-4 mb-3">
+            <div class="card z-index-2 ">
+            <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2 bg-transparent">
+                <div class="bg-gradient-dark shadow-dark border-radius-lg py-3 pe-1">
+                <div class="chart">
+                    <canvas id="chart-line-tasks" class="chart-canvas" height="170"></canvas>
+                </div>
+                </div>
+            </div>
+            <div class="card-body">
+                <h6 class="mb-0 ">Completed Tasks</h6>
+                <p class="text-sm ">Last Campaign Performance</p>
+                <hr class="dark horizontal">
+                <div class="d-flex ">
+                <i class="material-icons text-sm my-auto me-1">schedule</i>
+                <p class="mb-0 text-sm">just updated</p>
+                </div>
+            </div>
+            </div>
+        </div>
+        </div>
+        <div class="card mb-4">
             <div class="d-flex">
             <div class="icon icon-shape icon-lg bg-gradient-success shadow text-center border-radius-xl mt-n3 ms-4">
                 <i class="material-icons opacity-10" aria-hidden="true">language</i>
@@ -149,138 +281,6 @@
                 </div>
             </div>
             </div>
-        </div>
-        <div class="row mb-4">
-        <div class="col-lg-4 col-md-6 mt-4 mb-4">
-            <div class="card z-index-2 ">
-            <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2 bg-transparent">
-                <div class="bg-gradient-primary shadow-primary border-radius-lg py-3 pe-1">
-                <div class="chart">
-                    <canvas id="chart-bars" class="chart-canvas" height="170"></canvas>
-                </div>
-                </div>
-            </div>
-            <div class="card-body">
-                <h6 class="mb-0 ">Website Views</h6>
-                <p class="text-sm ">Last Campaign Performance</p>
-                <hr class="dark horizontal">
-                <div class="d-flex ">
-                <i class="material-icons text-sm my-auto me-1">schedule</i>
-                <p class="mb-0 text-sm"> campaign sent 2 days ago </p>
-                </div>
-            </div>
-            </div>
-        </div>
-        <div class="col-lg-4 col-md-6 mt-4 mb-4">
-            <div class="card z-index-2  ">
-            <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2 bg-transparent">
-                <div class="bg-gradient-success shadow-success border-radius-lg py-3 pe-1">
-                <div class="chart">
-                    <canvas id="chart-line" class="chart-canvas" height="170"></canvas>
-                </div>
-                </div>
-            </div>
-            <div class="card-body">
-                <h6 class="mb-0 "> Daily Sales </h6>
-                <p class="text-sm "> (<span class="font-weight-bolder">+15%</span>) increase in today sales. </p>
-                <hr class="dark horizontal">
-                <div class="d-flex ">
-                <i class="material-icons text-sm my-auto me-1">schedule</i>
-                <p class="mb-0 text-sm"> updated 4 min ago </p>
-                </div>
-            </div>
-            </div>
-        </div>
-        <div class="col-lg-4 mt-4 mb-3">
-            <div class="card z-index-2 ">
-            <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2 bg-transparent">
-                <div class="bg-gradient-dark shadow-dark border-radius-lg py-3 pe-1">
-                <div class="chart">
-                    <canvas id="chart-line-tasks" class="chart-canvas" height="170"></canvas>
-                </div>
-                </div>
-            </div>
-            <div class="card-body">
-                <h6 class="mb-0 ">Completed Tasks</h6>
-                <p class="text-sm ">Last Campaign Performance</p>
-                <hr class="dark horizontal">
-                <div class="d-flex ">
-                <i class="material-icons text-sm my-auto me-1">schedule</i>
-                <p class="mb-0 text-sm">just updated</p>
-                </div>
-            </div>
-            </div>
-        </div>
-        </div>
-        <div class="row">
-        <div class="col-lg-3 col-md-6 col-sm-6">
-            <div class="card  mb-2">
-            <div class="card-header p-3 pt-2">
-                <div class="icon icon-lg icon-shape bg-gradient-dark shadow-dark shadow text-center border-radius-xl mt-n4 position-absolute">
-                <i class="material-icons opacity-10">weekend</i>
-                </div>
-                <div class="text-end pt-1">
-                <p class="text-sm mb-0 text-capitalize">Bookings</p>
-                <h4 class="mb-0">281</h4>
-                </div>
-            </div>
-            <hr class="dark horizontal my-0">
-            <div class="card-footer p-3">
-                <p class="mb-0"><span class="text-success text-sm font-weight-bolder">+55% </span>than last week</p>
-            </div>
-            </div>
-        </div>
-        <div class="col-lg-3 col-md-6 col-sm-6 mt-sm-0 mt-4">
-            <div class="card  mb-2">
-            <div class="card-header p-3 pt-2">
-                <div class="icon icon-lg icon-shape bg-gradient-primary shadow-primary shadow text-center border-radius-xl mt-n4 position-absolute">
-                <i class="material-icons opacity-10">leaderboard</i>
-                </div>
-                <div class="text-end pt-1">
-                <p class="text-sm mb-0 text-capitalize">Today's Users</p>
-                <h4 class="mb-0">2,300</h4>
-                </div>
-            </div>
-            <hr class="dark horizontal my-0">
-            <div class="card-footer p-3">
-                <p class="mb-0"><span class="text-success text-sm font-weight-bolder">+3% </span>than last month</p>
-            </div>
-            </div>
-        </div>
-        <div class="col-lg-3 col-md-6 col-sm-6 mt-lg-0 mt-4">
-            <div class="card  mb-2">
-            <div class="card-header p-3 pt-2 bg-transparent">
-                <div class="icon icon-lg icon-shape bg-gradient-success shadow-success text-center border-radius-xl mt-n4 position-absolute">
-                <i class="material-icons opacity-10">store</i>
-                </div>
-                <div class="text-end pt-1">
-                <p class="text-sm mb-0 text-capitalize ">Revenue</p>
-                <h4 class="mb-0 ">34k</h4>
-                </div>
-            </div>
-            <hr class="horizontal my-0 dark">
-            <div class="card-footer p-3">
-                <p class="mb-0 "><span class="text-success text-sm font-weight-bolder">+1% </span>than yesterday</p>
-            </div>
-            </div>
-        </div>
-        <div class="col-lg-3 col-md-6 col-sm-6 mt-lg-0 mt-4">
-            <div class="card ">
-            <div class="card-header p-3 pt-2 bg-transparent">
-                <div class="icon icon-lg icon-shape bg-gradient-info shadow-info text-center border-radius-xl mt-n4 position-absolute">
-                <i class="material-icons opacity-10">person_add</i>
-                </div>
-                <div class="text-end pt-1">
-                <p class="text-sm mb-0 text-capitalize ">Followers</p>
-                <h4 class="mb-0 ">+91</h4>
-                </div>
-            </div>
-            <hr class="horizontal my-0 dark">
-            <div class="card-footer p-3">
-                <p class="mb-0 ">Just updated</p>
-            </div>
-            </div>
-        </div>
         </div>
         <div class="row mt-5">
         <div class="col-lg-4 col-md-6">

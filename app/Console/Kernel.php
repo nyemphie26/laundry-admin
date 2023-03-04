c18110daa0ebed32ce69902fe096015df1ccc35b
nyemphie26 <s.hidayatullah1726@gmail.com>
2023-03-04 10:28:40 +0700

    Add token expiration and scheduling sanctum prune

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -16,6 +17,10 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->command('sanctum:prune-expired --hours=24')->daily();
+        // $schedule->call(function () {
+        //     DB::table('order_trackers')->insert([]);
+        // })->daily();
     }
 
     /**
